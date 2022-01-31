1ee72983a95d5d80062502bac490dc304c80c72d
lucas <lucas@10.10.41.55>
2022-01-31 11:34:24 +0100

    Add img to calcul résistance.xml

--- resistances-javafx/src/main/java/com/mycompany/resistances/javafx/PrimaryController.java
+++ resistances-javafx/src/main/java/com/mycompany/resistances/javafx/PrimaryController.java
@@ -1,12 +1,21 @@
 package com.mycompany.resistances.javafx;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Rectangle rectangle1;
+    @FXML
+    private ImageView image;
+    
+    public void initialize(URL location, ResourceBundle resources) {
+        image.setVisible(true);
     }
+   
 }
--- resistances-javafx/src/main/java/module-info.java
+++ resistances-javafx/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.resistances.javafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.resistances.javafx to javafx.fxml;
     exports com.mycompany.resistances.javafx;
